c82028be1028ac4e8417ddfd7eedc6f91b8c3f9c
David Diaz <dadiaz@hp.com>
2015-09-10 17:40:06 -0600

    new: dev: implement mininet plugin @wip

--- lib/topology/platforms/mininet.py
+++ lib/topology/platforms/mininet.py
@@ -25,15 +25,74 @@ from __future__ import unicode_literals, absolute_import
 from __future__ import print_function, division
 
 import logging
+from mininet.net import Mininet
 
-from .base import BasePlatorm
+from .base import BasePlatform, BaseNode
 
 
 log = logging.getLogger(__name__)
 
 
-class MininetPlatform(BasePlatorm):
-    pass
+class MininetPlatform(BasePlatform):
+
+    def __init__(self, timestamp, nmlmanager):
+        super(MininetPlatform, self).__init__(timestamp, nmlmanager)
+        self._net = None
+        self.nmlnode_node_map = {}
+
+    def pre_build(self):
+        self._net = Mininet()
+        self._net.addController('c0')
+
+    def add_node(self, node):
+        variety = node.metadata.get('variety', 'switch')
+        mininet_node = None
+
+        if variety == 'switch':
+            mininet_node = MininetSwitch(
+                self._net.addSwitch(node.identifier,
+                                    dpid=str(len(self.nmlnode_node_map))))
+        elif variety == 'host':
+            mininet_node = MininetHost(self._net.addHost(node.identifier,
+                                       dpid=str(len(self.nmlnode_node_map))))
+        else:
+            log.error('Unsupported variety')
+
+        self.nmlnode_node_map[node.identifier] = mininet_node
+        return mininet_node
+
+    def add_biport(self, node, biport):
+        # For mininet this is not necessary
+        pass
+
+    def add_bilink(self, nodeport_a, nodeport_b, bilink):
+        node_a = self.nmlnode_node_map[nodeport_a[0].identifier].node
+        node_b = self.nmlnode_node_map[nodeport_b[0].identifier].node
+
+        self._net.addLink(node_a, node_b)
+
+    def post_build(self):
+        self._net.start()
+
+    def destroy(self):
+        self._net.stop()
 
 
-__all__ = ['MininetPlatform']
+class MininetSwitch(BaseNode):
+
+    def __init__(self, mininet_node):
+        self.node = mininet_node
+
+    def send_command(self, command):
+        self.node.sendCmd(command)
+
+
+class MininetHost(BaseNode):
+
+    def __init__(self, mininet_node):
+        self.node = mininet_node
+
+    def send_command(self, command):
+        self.node.sendCmd(command)
+
+__all__ = ['MininetPlatform', 'MininetSwitch']
