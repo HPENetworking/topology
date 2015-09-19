f2f3a1302fddd5575400b53289d1399e3df1e6dd
Carlos Miguel Jenkins Perez <carlos.jenkins@hp.com>
2015-09-18 23:17:39 -0600

    new: dev: Added a test to test all the build workflow of a TopologyManager.

--- test/test_topology_manager.py
+++ test/test_topology_manager.py
@@ -24,6 +24,8 @@ See http://pythontesting.net/framework/pytest/pytest-introduction/#fixtures
 from __future__ import unicode_literals, absolute_import
 from __future__ import print_function, division
 
+from os import system, getuid
+from distutils.spawn import find_executable
 from collections import OrderedDict
 
 import pytest  # noqa
@@ -43,6 +45,17 @@ sw1:1 -- hs1:1
 sw1: -- hs1
 [attr1=1] sw1:4 -- hs2
 """
+
+
+def teardown_function(function):
+    """
+    In case something goes wrong clean up mininet state.
+    """
+    if getuid() != 0:
+        return
+    mn_exec = find_executable('mn')
+    if mn_exec is not None:
+        system(mn_exec + ' --clean')
 
 
 def test_txtmeta_parse():
@@ -85,3 +98,34 @@ def test_txtmeta_parse():
 
     ddiff = DeepDiff(dictmeta, expected)
     assert not ddiff
+
+
+@pytest.mark.skipif(getuid() != 0, reason="Mininet requires root permissions")
+def test_build():
+    """
+    Test building and unbuilding a topology using the Mininet engine.
+    """
+    topology = TopologyManager(engine='mininet')
+
+    # Create topology using the NMLManager
+    sw1 = topology.nml.create_node(identifier='sw1', name='My Switch 1')
+    sw2 = topology.nml.create_node(identifier='sw2', name='My Switch 2')
+
+    sw1p1 = topology.nml.create_biport(sw1)
+    sw1p2 = topology.nml.create_biport(sw1)
+    sw1p3 = topology.nml.create_biport(sw1)  # noqa
+    sw2p1 = topology.nml.create_biport(sw2)
+    sw2p2 = topology.nml.create_biport(sw2)
+    sw2p3 = topology.nml.create_biport(sw2)  # noqa
+
+    sw1p1_sw2p1 = topology.nml.create_bilink(sw1p1, sw2p1)  # noqa
+    sw1p2_sw2p2 = topology.nml.create_bilink(sw1p2, sw2p2)  # noqa
+
+    # Build topology
+    topology.build()
+
+    # FIXME: More assert to check that the topology was build
+    assert topology.get('sw1') is not None
+
+    # Unbuild topology
+    topology.unbuild()
