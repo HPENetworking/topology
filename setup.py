280962966e75592aa781eb65a0c95110d88b690c
Carlos Miguel Jenkins Perez <carlos.jenkins@hpe.com>
2016-01-05 19:41:19 -0600

    chg: dev: Removing Toxin related node and test as it will not be part of the first public release.

--- setup.py
+++ setup.py
@@ -96,7 +96,6 @@ setup(
         ],
         'topology_docker_node_10': [
             'host = topology_docker.nodes.host:HostNode',
-            'toxin = topology_docker.nodes.toxin:ToxinNode',
             'openswitch = topology_docker.nodes.openswitch:OpenSwitchNode',
             'openvswitch = topology_docker.nodes.openvswitch:OpenvSwitchNode',
             'p4switch = topology_docker.nodes.p4switch:P4SwitchNode',
