b96bbc11cddb4b7adc1afc3b2bf72fc158f8e36f
Carlos Miguel Jenkins Perez <carlos.jenkins@hpe.com>
2016-03-22 21:51:49 -0600

    chg: dev: Making the new theme official.

--- doc/conf.py
+++ doc/conf.py
@@ -11,9 +11,9 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import os
 from os.path import join, dirname, abspath
 from topology import __version__
+from guzzle_sphinx_theme import html_theme_path
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -112,33 +112,21 @@ todo_include_todos = False
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'default'
+# Adds an HTML table visitor to apply Bootstrap table classes
+html_translator_class = 'guzzle_sphinx_theme.HTMLTranslator'
+html_theme = 'guzzle_sphinx_theme'
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-# html_theme_options = {}
+# Register the theme as an extension to generate a sitemap.xml
+extensions.append('guzzle_sphinx_theme')
+
+# Guzzle theme options (see theme.conf for more information)
+html_theme_options = {
+    # Set the name of the project to appear in the sidebar
+    'project_nav_name': 'Home'
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = []
-
-# The name for this set of Sphinx documents.  If None, it defaults to
-# "<project> v<release> documentation".
-# html_title = None
-
-# A shorter title for the navigation bar.  Default is the same as html_title.
-# html_short_title = None
-
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
-# html_logo = None
-
-# The name of an image file (within the static path) to use as favicon of the
-# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
-# pixels large.
-# html_favicon = None
+html_theme_path = html_theme_path()
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -335,22 +323,3 @@ graphviz_output_format = 'svg'
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3.4', None)
 }
-
-# Setup theme if not building in readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) is not None
-if not on_rtd:
-    import guzzle_sphinx_theme
-
-    # Adds an HTML table visitor to apply Bootstrap table classes
-    html_translator_class = 'guzzle_sphinx_theme.HTMLTranslator'
-    html_theme_path = guzzle_sphinx_theme.html_theme_path()
-    html_theme = 'guzzle_sphinx_theme'
-
-    # Register the theme as an extension to generate a sitemap.xml
-    extensions.append('guzzle_sphinx_theme')
-
-    # Guzzle theme options (see theme.conf for more information)
-    html_theme_options = {
-        # Set the name of the project to appear in the sidebar
-        'project_nav_name': 'Topology Network Framework'
-    }
