01b0af98396a47cf4bcd597385e90d882b0e3880
Carlos Jenkins <carlos.jenkins@hpe.com>
2019-11-14 17:43:35 -0600

    chg: dev: Removing bad shebang for Python 2.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2015-2016 Hewlett Packard Enterprise Development LP
+# Copyright (C) 2015-2019 Hewlett Packard Enterprise Development LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
